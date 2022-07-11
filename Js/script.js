cbbac2f73b4d14cfb5ec2aba16c033d02992bc54
ullah <sheikholeei97@gmail.ocm>
2022-07-11 21:17:54 +0600

    about section + issue fixed

--- Js/script.js
+++ Js/script.js
@@ -5,3 +5,9 @@ var typed = new Typed('.typing',{
     typeSpeed: 80,
     backSpeed:80
 });
+
+var typed = new Typed('.typing-2',{
+    strings: ['Web Developer', 'blogger', 'entepreneur', 'youtuber'],
+    typeSpeed: 80,
+    backSpeed:80
+});
