a69bf3423c5143b664121c34846d40818bffbf58
ullah <sheikholeei97@gmail.ocm>
2022-07-11 18:13:22 +0600

    nav and header done

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+// Automatic word writing effect with type script
+
+var typed = new Typed('.typing',{
+    strings: ['Web Developer', 'Youtuber', 'blogger', 'entepreneur'],
+    typeSpeed: 80,
+    backSpeed:80
+});
